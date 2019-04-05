d4a6518f3fd0edb26e17ec1cf7ac9b966c57229a
demel42 <demel42@web.de>
2019-04-05 11:57:22 +0200

    1.8

--- GoogeMaps/module.php
+++ GoogeMaps/module.php
@@ -151,8 +151,9 @@ class GoogleMaps extends IPSModule
     private function getMyLocation()
     {
         $id = IPS_GetInstanceListByModuleID('{45E97A63-F870-408A-B259-2933F7EABF74}')[0];
-        $lat = IPS_GetProperty($id, 'Latitude');
-        $lng = IPS_GetProperty($id, 'Longitude');
+		$loc = json_decode(IPS_GetProperty($id, 'Location'));
+		$lat = $loc->latitude;
+		$lng = $loc->longitude;
         $loc = json_encode(['lng' => $lng, 'lat' => $lat]);
         return $loc;
     }
