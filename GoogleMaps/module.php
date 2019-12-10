3957989568173cd0f2c6969ad1cc62c1fb2abd36
IP-Symcon <root@ips-prod>
2019-12-10 17:31:21 +0100

    GenerateDynamicMap() kann nun auch "DirectionServices"

--- GoogleMaps/module.php
+++ GoogleMaps/module.php
@@ -200,6 +200,7 @@ class GoogleMaps extends IPSModule
         $infowindow_options = isset($map['infowindow_options']) ? $map['infowindow_options'] : '';
         $markers = isset($map['markers']) ? $map['markers'] : '';
         $paths = isset($map['paths']) ? $map['paths'] : '';
+        $directions = isset($map['directions']) ? $map['directions'] : '';
         $layers = isset($map['layers']) ? $map['layers'] : '';
 
         // Kopf
@@ -304,21 +305,124 @@ class GoogleMaps extends IPSModule
             }
         }
 
+        // Karte mit Routen
+        if ($directions != '') {
+            $request = [];
+            if (isset($directions['origin'])) {
+                if (isset($directions['origin']['lat']) && isset($directions['origin']['lng'])) {
+                    $lat = $this->format_float((float) $directions['origin']['lat'], 6);
+                    $lng = $this->format_float((float) $directions['origin']['lng'], 6);
+                    $origin = $lat . ',' . $lng;
+                } else {
+                    $origin = $directions['origin'];
+                }
+                $request['origin'] = $origin;
+            }
+            if (isset($directions['destination'])) {
+                if (isset($directions['destination']['lat']) && isset($directions['destination']['lng'])) {
+                    $lat = $this->format_float((float) $directions['destination']['lat'], 6);
+                    $lng = $this->format_float((float) $directions['destination']['lng'], 6);
+                    $destination = $lat . ',' . $lng;
+                } else {
+                    $destination = $directions['destination'];
+                }
+                $request['destination'] = $destination;
+            }
+
+            $travelMode = strtoupper($this->GetArrayElem($directions, 'travelMode', 'DRIVING'));
+            if (!in_array($travelMode, ['DRIVING', 'BICYCLING', 'TRANSIT', 'WALKING'])) {
+                $this->LogMessage(__FUNCTION__ . ': unknown travelMode "' . $travelMode . '"', KL_WARNING);
+                $travelMode = 'DRIVING';
+            }
+            $request['travelMode'] = $travelMode;
+
+            $request['provideRouteAlternatives'] = (bool) $this->GetArrayElem($directions, 'provideRouteAlternatives', false);
+
+            $request['avoidTolls'] = (bool) $this->GetArrayElem($directions, 'avoidTolls', false);
+            $request['avoidFerries'] = (bool) $this->GetArrayElem($directions, 'avoidFerries', false);
+            $request['avoidHighways'] = (bool) $this->GetArrayElem($directions, 'avoidHighways', false);
+
+            if (isset($directions['drivingOptions'])) {
+                $o = $directions['drivingOptions'];
+                if (isset($o['departureTime'])) {
+                    if (is_numeric($o['departureTime'])) {
+                        $t = $o['departureTime'];
+                        $o['departureTime'] = date('r', $t);
+                    }
+                } else {
+                    $o['departureTime'] = date('r');
+                }
+                $request['drivingOptions'] = $o;
+            }
+            if (isset($directions['transitOptions'])) {
+                $o = $directions['transitOptions'];
+                if (isset($o['arrivalTime'])) {
+                    if (is_numeric($o['arrivalTime'])) {
+                        $t = $o['arrivalTime'];
+                        $o['arrivalTime'] = date('r', $t);
+                    }
+                } elseif (isset($o['departureTime'])) {
+                    if (is_numeric($o['departureTime'])) {
+                        $t = $o['departureTime'];
+                        $o['departureTime'] = date('r', $t);
+                    }
+                } else {
+                    $o['departureTime'] = date('r');
+                }
+                if (isset($o['modes'])) {
+                    $md = [];
+                    foreach ($o['modes'] as $m) {
+                        $md[] = strtoupper($m);
+                    }
+                    $o['modes'] = $md;
+                }
+                if (isset($o['routingPreference'])) {
+                    $o['routingPreference'] = strtoupper($o['routingPreference']);
+                }
+                $request['transitOptions'] = $o;
+            }
+
+            $html .= '
+			var directionsService = new google.maps.DirectionsService();
+			var directionsRenderer = new google.maps.DirectionsRenderer();
+			directionsRenderer.setMap(map);
+			var request = ' . json_encode($request) . ';';
+            if (isset($request['drivingOptions']['departureTime'])) {
+                $html .= '
+			request["drivingOptions"]["departureTime"] = new Date("' . $request['drivingOptions']['departureTime'] . '");';
+            }
+            if (isset($request['transitOptions']['arrivalTime'])) {
+                $html .= '
+			request["transitOptions"]["arrivalTime"] = new Date("' . $request['transitOptions']['arrivalTime'] . '");';
+            }
+            if (isset($request['transitOptions']['departureTime'])) {
+                $html .= '
+			request["transitOptions"]["departureTime"] = new Date("' . $request['transitOptions']['departureTime'] . '");';
+            }
+            $html .= '
+			directionsService.route(request, function(result, status) {
+				if (status == "OK") {
+					directionsRenderer.setDirections(result);
+				}
+			});
+';
+        }
+
         if ($layers != '') {
             foreach ($layers as $layer) {
                 switch ($layer) {
                     case 'traffic':
                         $html .= '
-                var trafficLayer = new google.maps.TrafficLayer();
-                trafficLayer.setMap(map);
+				var trafficLayer = new google.maps.TrafficLayer();
+				trafficLayer.setMap(map);
 ';
                         break;
                     case 'transit':
                         $html .= '
-                var transitLayer = new google.maps.TransitLayer();
-                transitLayer.setMap(map);
+				var transitLayer = new google.maps.TransitLayer();
+				transitLayer.setMap(map);
 ';
-                        break;
+                            break;
 
                     case 'bike':
                         $html .= '
